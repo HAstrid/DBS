3f4a5e4ba0fa38081d4d6d4aed6213cccd32d400
anujnaRao <anujna.p.rao@gmail.com>
2019-06-13 18:42:31 +0530

    init

--- DB3_2.sql
+++ DB3_2.sql
@@ -2,4 +2,8 @@ delete from affiliated_with where phid='5';
  
  select * from affiliated_with;
  
- 
+ select * from medication;
+select * from Appointment;
+
+select p.name,mname from Medication m, Patient p,physician ph, prescribe s,appointment a where
+ m.code=s.code and ph.phid=s.phid and p.ptid =s.ptid and p.phid=ph.phid and p.ptid=a.ptid and ph.phid=a.phid and p.ptid != all(select ptid from appointment);
